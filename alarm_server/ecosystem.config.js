772e706aba0e08cc6ff053c5d17e4c469f0a3867
Ethan Dean <ethandean7@gmail.com>
2025-12-26 12:31:21 -0500

    fix: run pm2 correctly

--- alarm_server/ecosystem.config.js
+++ alarm_server/ecosystem.config.js
@@ -1,16 +1,15 @@
 module.exports = {
   apps: [{
     name: 'alarms_prod',
-    script: 'venv/bin/uvicorn',
-    args: 'main:app --host 127.0.0.1 --port 5002',
-    cwd: '/deployments/alarm_server',
+    script: 'main.py',
+    interpreter: 'venv/bin/python3',
+    cwd: '/root/deployments/alarm_server',
     instances: 1,
     exec_mode: 'fork',
     autorestart: true,
     watch: false,
     max_memory_restart: '100M',
     env: {
-      NODE_ENV: 'production',
       PYTHONUNBUFFERED: '1'
     },
     error_file: './logs/pm2-error.log',
